e16ea292f72ed564c8e45394e0031d35c81aa908
康建城 <1063303665@qq.com>
2020-12-18 19:01:41 +0800

    d

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 ## imart
 一个 JavaScript 常用工具库
+
+
+签名方式：
+
+```
+pkgsign sign --signer pgp --pgp-private-key-path ./openpgp_prikey.txt --pgp-private-key-passphrase kjc_true --pgp-public-key-https-url https://keybase.io/kjc233/pgp_keys.asc .
+```
+
+验证方式：
+
+```
+pkgsign verify . --full
+```
--- index.js
+++ index.js
@@ -1,2 +1,19 @@
+const openpgp = require('openpgp') // use as CommonJS, AMD, ES6 module or via window.openpgp
 
-console.log('Welcome imart home1!')
+openpgp.initWorker({ path: 'openpgp.worker.js' }); // set the relative web worker path
+
+(async () => {
+
+  /**
+   * ECC keys
+   */
+
+  const { privateKeyArmored, publicKeyArmored, revocationCertificate } = await openpgp.generateKey({
+    userIds: [{ name: 'kjc-true', email: 'kjc@example.com' }], // you can pass multiple user IDs
+    curve: 'ed25519',                                           // ECC curve name
+    passphrase: 'kjc_true'                                      // protects the private key
+  });
+
+  console.log('ecc-publicKeyArmored:\n', publicKeyArmored);      // '-----BEGIN PGP PUBLIC KEY BLOCK ... '
+  console.log('ecc-privateKeyArmored:\n', privateKeyArmored);     // '-----BEGIN PGP PRIVATE KEY BLOCK ... '
+})();
